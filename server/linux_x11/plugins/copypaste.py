f1872e6ccbb87725d0590d6da79567ebcb392a9d
Dane Finlay <Danesprite@gmail.com>
2018-11-27 01:04:38 +1100

    Adjust copypaste plugin to use pyperclip instead
    I've swapped the function names to be consistent with pyperclip's
    functions.

--- server/linux_x11/plugins/copypaste.py
+++ server/linux_x11/plugins/copypaste.py
@@ -1,23 +1,22 @@
 from yapsy.IPlugin import IPlugin
-from subprocess32 import check_output
+from subprocess32 import check_output, Popen, PIPE
 import os
 
 enabled = True
 
-def write_command(message, arguments='type --file -',
-                  executable=None):
-    with os.popen('%s %s' % (executable, arguments), 'w') as fd:
-        fd.write(message)
 
-def paste(text='default'):
-    write_command(text, arguments='-b', executable='xsel')
-    # server.key_press(key="v", modifiers=("control"))
+def copy(text):
+    with os.popen('python -m pyperclip -c', 'w') as fd:
+        fd.write(text)
 
-def copy():
-    text = check_output("xsel", shell=True)
+
+def paste():
+    text = check_output(['python', '-m', 'pyperclip', '-p'])
     return text
+
 
 class CopypastePlugin(IPlugin):
     def register_rpcs(self, server):
         server.register_function(paste)
         server.register_function(copy)
+
